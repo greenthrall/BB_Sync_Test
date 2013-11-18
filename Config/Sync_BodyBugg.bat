dbec8ba0f0d99eaf8667f4f5db21d4a47b5a5c27
Travis Hall <greenthrall@gmail.com>
2013-11-17 19:57:09 -0800

    Updated program launch location

--- Config/Sync_BodyBugg.bat
+++ Config/Sync_BodyBugg.bat
@@ -4,8 +4,8 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
 For /f "tokens=1-3 delims=/:" %%a in ("%time%") do (set mytime=%%a%%b%%c)
 echo on
 
-C:\Python\v2.7\python.exe C:\BodyMedia\bodymedia.py --fromSerial=COM3 --toCsv=%mydate%_%mytime:~0,6%.csv
+C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --toCsv=C:\Users\Travis\Documents\GitHub\Data\%mydate%_%mytime:~0,6%.csv
 
 PAUSE
 
-C:\Python\v2.7\python.exe C:\BodyMedia\bodymedia.py --fromSerial=COM3 --clear
+C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --clear
