a01ba023f538110837c9af3f9cfee55a091ff429
Travis Hall <greenthrall@gmail.com>
2013-12-12 23:31:43 -0800

    Fixed hour format to always be two digits

--- Config/Sync_BodyBugg.bat
+++ Config/Sync_BodyBugg.bat
@@ -1,10 +1,16 @@
 
-echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
-For /f "tokens=1-3 delims=/:" %%a in ("%time%") do (set mytime=%%a%%b%%c)
-echo on
+ECHO OFF
 
-C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --toCsv=C:\Users\Travis\Documents\GitHub\Data\%mydate%_%mytime:~0,6%.csv
+SET HOUR=%time:~0,2%
+SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+IF "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) ELSE (SET dtStamp=%dtStamp24%)
+
+ECHO %dtStamp%
+ECHO ON
+
+C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --toCsv=C:\Users\Travis\Documents\GitHub\Data\%dtStamp%.csv
 
 PAUSE
 
