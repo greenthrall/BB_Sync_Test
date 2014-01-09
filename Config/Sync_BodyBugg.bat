0496f8fa6e1c24b9739c46b0f43e8bb01809b098
Travis Hall <greenthrall@gmail.com>
2014-01-08 20:03:03 -0800

    More accurate date time format

--- Config/Sync_BodyBugg.bat
+++ Config/Sync_BodyBugg.bat
@@ -1,16 +1,12 @@
 
 ECHO OFF
 
-SET HOUR=%time:~0,2%
-SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2% 
-SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\Get_Current_Date_Time.py > tempfile
 
-IF "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) ELSE (SET dtStamp=%dtStamp24%)
+SET /p DATETIME= < tempfile
+DEL tempfile
 
-ECHO %dtStamp%
-ECHO ON
-
-C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --toCsv=C:\Users\Travis\Documents\GitHub\Data\%dtStamp%.csv
+C:\Python\v2.7\python.exe C:\Users\Travis\Documents\GitHub\BB_Sync_Test\Program\BodyBugg.py --fromSerial=COM3 --toCsv=C:\Users\Travis\Documents\GitHub\Data\%DATETIME%.csv
 
 PAUSE
 
---
+++ Program/Get_Current_Date_Time.py
@@ -0,0 +1,6 @@
+ 
+from datetime import datetime
+ 
+i = datetime.now()
+ 
+print i.strftime('%Y%m%d_%H%M%S')
